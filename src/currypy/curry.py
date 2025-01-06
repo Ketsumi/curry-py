8d5ae891ca1741bfa5723f2777227ab203094e72
Ketsumi <mewtropolis.ketsumi@gmail.com>
2025-01-06 09:07:33 +0000

    1. signature & __annotations__ are now retained. 2. kwargs are now possible. 3. added docstring

--- src/currypy/curry.py
+++ src/currypy/curry.py
@@ -1,12 +1,25 @@
-from functools import partial
+from functools import partial, wraps
 from inspect import signature
+from typing import Callable
 
-def curry(f):
 
-    def wrapper(*args):
-        if len(args) >= len(signature(f).parameters):
-            return f(*args)
+def curry(f: Callable):
+    """
+    Creates a new and curryable function from a Callable.
+    """
 
-        return curry(partial(f, *args))
+    def partial_signature_arguments(*args, **kwargs):
+        return signature(f).bind_partial(*args, **kwargs).arguments
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if len(partial_signature_arguments(*args, **kwargs)) >= len(
+            signature(f).parameters
+        ):
+            return f(*args, **kwargs)
+
+        return curry(wraps(f)(partial(f, *args, **kwargs)))
+
+    setattr(wrapper, "__signature__", signature(f))
 
     return wrapper
